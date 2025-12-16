3465bf264937d66683b513d5ef7f8ae31243007e
ngtv <tuanvy860@gmail.com>
2025-12-16 12:31:15 +0700

    add old path and make the build/ dir first in PATH

--- dev.sh
+++ dev.sh
@@ -5,4 +5,5 @@ export CDER_DB_PATH="$(realpath ./cder_db)"
 mkdir build
 cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Debug
 
-export PATH="$PATH:$(realpath ./build/)"
+export OLD_PATH="$PATH"
+export PATH="$(realpath ./build/):$PATH"
