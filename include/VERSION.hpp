c09d12682062bba57611985c675e12c01e65f788
ngtv <tuanvy860@gmail.com>
2025-12-16 12:31:49 +0700

    Change to version 1.0.0

--- include/VERSION.hpp
+++ include/VERSION.hpp
@@ -4,8 +4,8 @@ namespace cder {
     static constexpr int VersionMajor = 1;
     static constexpr int VersionMinor = 0;
     static constexpr int VersionPatch = 0;
-    static constexpr char ReleaseFlag[] = "a";
-    static constexpr char Version[] = "1.0.0a";
+    static constexpr char ReleaseFlag[] = "";
+    static constexpr char Version[] = "1.0.0";
 }
 
 #endif // !VERSION_HPP
